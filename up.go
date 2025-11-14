538a991f479cd397a4dbaa24a45ade9016de0247
sergeyv5577-hero <sergeyv5577@gmail.com>
2025-11-14 16:59:42 +0500

    Release 0.5

--- up.go
+++ up.go
@@ -39,7 +39,7 @@ import (
 	"github.com/spf13/pflag"
 )
 
-const version = "0.4 (2020-10-29)"
+const version = "0.5 (2025-11-14)"
 
 // TODO: in case of error, show it in red (bg?), then below show again initial normal output (see also #4)
 // TODO: F1 should display help, and it should be multi-line, and scrolling licensing credits
